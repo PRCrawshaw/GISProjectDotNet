6fac93f48a85236c2ce0563710d9aa957ab1f277
PRCrawshaw <crawshaw2014@gmail.com>
2018-04-23 12:30:39 +1200

    Initial commit

---
+++ GISProjectDotNet/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Write your JavaScript code.
+
+var map, infoWindow;
+var defaultLocation = {lat: -43.537923, lng: 172.643018};
+var markersArray = [];
+
+function AlertName(name)
+{
+    alert('You clicked '+ name +"!");
+}
+
+function initMap()
+{
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: defaultLocation
+        });
+        console.log('Zoom is: '+map.getZoom()); 
+}
+
+
